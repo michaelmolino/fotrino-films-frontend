845559052070f16a620b2c098920203449a53ab3
Michael Molino <creative@michaelmolino.com>
2022-01-05 17:22:29 +0000

    initial navbar changes

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -10,15 +10,25 @@
         to="/"
       />
     </q-toolbar-title>
-    <q-toggle
-      class="text-uppercase"
-      v-model="darkMode"
-      unchecked-icon="wb_sunny"
-      checked-icon="nights_stay"
-      :label="$q.screen.gt.xs ? 'Dark Mode' : ''"
-      color="secondary"
-      keep-color
-    />
+    <q-btn-dropdown icon="settings" label="Settings" flat no-caps size="lg">
+      <q-toggle
+        class="text-uppercase"
+        label="Dark Mode"
+        v-model="darkMode"
+        unchecked-icon="wb_sunny"
+        checked-icon="nights_stay"
+        color="secondary"
+        keep-color
+      />
+    </q-btn-dropdown>
+    <q-btn-dropdown icon="account_box" label="Login" flat no-caps size="lg">
+      <q-btn flat icon="facebook" label="Facebook" size="md" />
+      <q-separator />
+      <q-btn flat icon="mail" label="Gmail" size="md" />
+      <q-separator />
+      <q-btn flat icon="reddit" label="Reddit" size="md" />
+      <q-separator />
+    </q-btn-dropdown>
   </q-toolbar>
 </template>
 
