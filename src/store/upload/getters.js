52c0d2ac9a56cf0e38772ebe5da30dab2ed2de44
Michael Molino <creative@michaelmolino.com>
2025-02-23 22:37:43 +0000

    remove commented out code

--- src/store/upload/getters.js
+++
@@ -1,7 +0,0 @@
-// export function object(state) {
-//   return state.object
-// }
-
-// export function file(state) {
-//   return state.file
-// }
--- src/store/upload/mutations.js
+++
@@ -1,7 +0,0 @@
-// export function SET_OBJECT(state, object) {
-//   state.object = object
-// }
-
-// export function SET_FILE(state, file) {
-//   state.file = file
-// }
