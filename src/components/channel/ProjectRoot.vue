8f1c4d3cf06bfbf914b912db5a20ea4c83ea4677
Michael Molino <creative@michaelmolino.com>
2025-09-13 18:01:17 +0100

    fix spacing for projects with multiple featured videos

--- src/components/channel/ProjectRoot.vue
+++ src/components/channel/ProjectRoot.vue
@@ -21,7 +21,7 @@
         </div>
         <div class="row">
           <div
-            class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2"
+            class="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 q-pa-sm"
             v-for="media in project.media?.filter(ch => ch.id !== media.id)"
             :key="media.id"
           >
