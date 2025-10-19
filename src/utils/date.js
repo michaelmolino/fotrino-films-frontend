f7528eb07f258b546f0179c1f3feaa9fafff5cda
Michael Molino <creative@michaelmolino.com>
2025-10-19 17:49:09 +0100

    proper timezone fix

--- src/utils/date.js
+++ src/utils/date.js
@@ -2,14 +2,15 @@ import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import isToday from 'dayjs/plugin/isToday'
 import isYesterday from 'dayjs/plugin/isYesterday'
+import utc from 'dayjs/plugin/utc'
 
 dayjs.extend(relativeTime)
 dayjs.extend(isToday)
 dayjs.extend(isYesterday)
+dayjs.extend(utc)
 
 export function daysSince(start, withTime = true) {
-  // This implicitly converts from server UTC time to local time
-  const day = dayjs(new Date(start))
+  const day = dayjs.utc(start).local()
   if (!withTime) {
     if (day.isToday()) return 'today'
     if (day.isYesterday()) return 'yesterday'
