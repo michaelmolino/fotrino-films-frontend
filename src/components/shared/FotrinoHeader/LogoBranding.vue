02bdeb091c998b0d86a78da9cadf42a3ed84e81d
Michael Molino <creative@michaelmolino.com>
2026-06-01 21:28:35 +0100

    branding

--- src/components/shared/FotrinoHeader/LogoBranding.vue
+++ src/components/shared/FotrinoHeader/LogoBranding.vue
@@ -1,6 +1,6 @@
 <template>
   <q-btn
-    icon="img:/icons/078f3904c20a921e06f8a2796ed0b538854c1b3572b5b95fd6fe16ec455bdb49-32x32.png"
+    icon="play_circle"
     :label="buttonLabel"
     flat
     no-caps
